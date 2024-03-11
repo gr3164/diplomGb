7d2a07b6f7b9add1574a30c02d387e023bcc3840
Grisha <pin.pinigin@mail.ru>
2024-03-11 11:35:13 +0500

    desktop layout

--- gulpfile.js
+++ gulpfile.js
@@ -16,99 +16,106 @@ const svgSprite = require('gulp-svg-sprite');
 const include = require('gulp-include');
 
 function pages() {
-    return src('app/pages/*.html')
-        .pipe(include({
-            includePaths: 'app/components'
-        }))
-        .pipe(dest('app'))
-        .pipe(browserSync.stream())
+  return src('app/pages/*.html')
+    .pipe(
+      include({
+        includePaths: 'app/components',
+      }),
+    )
+    .pipe(dest('app'))
+    .pipe(browserSync.stream());
 }
 
 function fonts() {
-    return src('app/fonts/src/*.*')
-        .pipe(fonter({
-            formats: ['woff', 'ttf']
-        }))
-        .pipe(src('app/fonts/*.ttf'))
-        .pipe(ttf2woff2())
-        .pipe(dest('app/fonts'))
+  return src('app/fonts/src/*.*')
+    .pipe(
+      fonter({
+        formats: ['woff', 'ttf'],
+      }),
+    )
+    .pipe(src('app/fonts/*.ttf'))
+    .pipe(ttf2woff2())
+    .pipe(dest('app/fonts'));
 }
 
 function images() {
-    return src(['app/images/src/*.*', '!app/images/src/*.svg'])
-        .pipe(newer('app/images'))
-        .pipe(avif({ quality: 50 }))
+  return src(['app/images/src/*.*', '!app/images/src/*.svg'])
+    .pipe(newer('app/images'))
+    .pipe(avif({ quality: 50 }))
 
-        .pipe(src('app/images/src/*.*'))
-        .pipe(newer('app/images'))
-        .pipe(webp())
+    .pipe(src('app/images/src/*.*'))
+    .pipe(newer('app/images'))
+    .pipe(webp())
 
-        .pipe(src('app/images/src/*.*'))
-        .pipe(newer('app/images'))
-        .pipe(imagemin())
+    .pipe(src('app/images/src/*.*'))
+    .pipe(newer('app/images'))
+    .pipe(imagemin())
 
-        .pipe(dest('app/images'))
+    .pipe(dest('app/images'));
 }
 
 function sprite() {
-    return src('app/images/*.svg')
-        .pipe(svgSprite({
-            mode: {
-                stack: {
-                    sprite: '../sprite.svg',
-                    example: true
-                }
-            }
-        }))
-        .pipe(dest('app/images'))
+  return src('app/images/*.svg')
+    .pipe(
+      svgSprite({
+        mode: {
+          stack: {
+            sprite: '../sprite.svg',
+            example: true,
+          },
+        },
+      }),
+    )
+    .pipe(dest('app/images'));
 }
 
 function scripts() {
-    return src('app/js/main.js')
-        .pipe(autoprefixer({ overrideBrowserslist: ['last 10 version'] }))
-        .pipe(concat('main.min.js'))
-        .pipe(uglify())
-        .pipe(dest('app/js'))
-        .pipe(browserSync.stream())
+  return src('app/js/main.js')
+    .pipe(autoprefixer({ overrideBrowserslist: ['last 10 version'] }))
+    .pipe(concat('main.min.js'))
+    .pipe(uglify())
+    .pipe(dest('app/js'))
+    .pipe(browserSync.stream());
 }
 
 function styles() {
-    return src('app/scss/style.scss')
-        .pipe(concat('style.min.css'))
-        .pipe(scss({ outputStyle: 'compressed' }))
-        .pipe(dest('app/css'))
-        .pipe(browserSync.stream())
+  return src('app/scss/style.scss')
+    .pipe(concat('style.min.css'))
+    .pipe(scss({ outputStyle: 'compressed' }))
+    .pipe(dest('app/css'))
+    .pipe(browserSync.stream());
 }
 
 function watching() {
-    browserSync.init({
-        server: {
-            baseDir: "app/"
-        }
-    });
-    watch(['app/scss/style.scss'], styles)
-    watch(['app/images/src'], images)
-    watch(['app/js/main.js'], scripts)
-    watch(['app/components/*', 'app/pages/*'], pages)
-    watch(['app/*.html']).on('change', browserSync.reload);
+  browserSync.init({
+    server: {
+      baseDir: 'app/',
+    },
+  });
+  watch(['app/scss/*.scss'], styles);
+  watch(['app/images/src'], images);
+  watch(['app/js/main.js'], scripts);
+  watch(['app/components/*', 'app/pages/*'], pages);
+  watch(['app/*.html']).on('change', browserSync.reload);
 }
 
 function cleanDist() {
-    return src('dist')
-        .pipe(clean())
+  return src('dist').pipe(clean());
 }
 
 function building() {
-    return src([
-        'app/css/style.min.css',
-        'app/images/*.*',
-        '!app/images/*.svg',
-        'app/images/sprite.svg',
-        'app/fonts/*.*',
-        'app/js/main.min.js',
-        'app/**/*.html'
-    ], { base: 'app' })
-        .pipe(dest('dist'))
+  return src(
+    [
+      'app/css/style.min.css',
+      'app/images/*.*',
+      '!app/images/*.svg',
+      'app/images/sprite.svg',
+      'app/fonts/*.*',
+      'app/js/main.min.js',
+      'app/**/*.html',
+    ],
+    { base: 'app' },
+  ).pipe(dest('dist'));
 }
 
 exports.styles = styles;
@@ -119,7 +126,6 @@ exports.pages = pages;
 exports.scripts = scripts;
 exports.building = building;
 exports.watching = watching;
-
 
 exports.build = series(cleanDist, building);
 exports.default = parallel(styles, images, scripts, pages, watching);
